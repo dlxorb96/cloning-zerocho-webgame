530bd09440d850b1d641e70e5da71afb367b75d3
dlxorb96 <80591023+dlxorb96@users.noreply.github.com>
2021-08-08 23:47:35 +0900

    8/8 공부를 통해 배운점
    1. arrow fucntion () => 띄어쓰기 주의 화살표 사이에 띄어썼다가 한참을 해맸다.
    
    2. if문 안에 if문이 많을 때 해결하는 방법
      -1. 공통된 절차를 각 분기점 내부에 넣는다.
      -2. 분기점에서 짧은 절차부터 실행.
      -3. 짧은 절차가 끝나면 return(함수 내부의 경우)이나 break(for문 내부의 경우)로 중단한다. 
      -4. else를 제거한다. ( 이때 중첩하나가 제거 된다.)
    
    if 안에 if가 있으면 길어짐 이를 앞으로 보냄으로써 if를 else로 보낸다 else{ if()}이런 모양이겠지
    그리고 return으로 함수를 끝내버리고, 새로운 if가 else가 사라진채로 들여쓰기 하나를 지울 수 있게 되는거임.
    
    
    막힌 부분
    result를 원래 operator가 사용될 때 ''상태로 만들었는데 number2가 쓰일때로 바꾸려고 한다.
    이를 내일 해결해보자

--- 계산기/main.js
+++ 계산기/main.js
@@ -6,14 +6,14 @@ let secondNumber ='';
 let operator='';
 
 
-const onclickNumber = (number) => () => {
-        if(!operator){
-            firstNumber += number
-        }
-        else{
-            secondNumber += number
-        }
-        result.value += number;}
+// const onclickNumber = (number) => () => {
+//         if(!operator){
+//             firstNumber += number
+//         }
+//         else{
+//             secondNumber += number
+//         }
+//         result.value += number;}
     
 
 
@@ -27,49 +27,44 @@ const onclickNumber = (number) => () => {
 //     result.value += number;};
 // }
 
-document.querySelector('#number1').addEventListener('click',onclickNumber('1'))
-document.querySelector('#number2').addEventListener('click', onclickNumber('2'))
-document.querySelector('#number3').addEventListener('click', onclickNumber('3'))
-document.querySelector('#number4').addEventListener('click', onclickNumber('4'))
-document.querySelector('#number5').addEventListener('click', onclickNumber('5'))
-document.querySelector('#number6').addEventListener('click', onclickNumber('6'))
-document.querySelector('#number7').addEventListener('click', onclickNumber('7'))
-document.querySelector('#number8').addEventListener('click', onclickNumber('8'))
-document.querySelector('#number9').addEventListener('click', onclickNumber('9'))
-document.querySelector('#number0').addEventListener('click', onclickNumber('0'))
 
-document.querySelector('#plus').addEventListener('click', () => 
-{if(!secondNumber)
-    {operator = '+';
-    printOperator.value = '+';
-    result.value = '';
+
+const onclickNumber = (event) =>
+        
+    {if(!operator)
+        {firstNumber += event.target.textContent}
+    else
+        {if(secondNumber){
+            result.value = '';
+        }
+        secondNumber+= event.target.textContent};
+        result.value += event.target.textContent;}
+    
+const onclickOperator = (op) => () => 
+    {if(firstNumber){
+        operator = op
+        printOperator.value = op
+        // result.value = ''
     }
-else
-    {alert('error!');}})
-document.querySelector('#minus').addEventListener('click', () =>
-{if(!secondNumber)
-    {operator = '-';
-    printOperator.value = '-';
-    result.value = '';
+    else{
+        alert('error!');}
     }
-else
-    {alert('error!');}})
-document.querySelector('#multiply').addEventListener('click', () => 
-{if(!secondNumber)
-    {operator = '*';
-    printOperator.value = '*';
-    result.value = '';
-    }
-else
-    {alert('error!');}})
-document.querySelector('#divide').addEventListener('click', () => 
-{if(!secondNumber)
-    {operator = '/';
-    printOperator.value = '/';
-    result.value = '';
-    }
-else
-    {alert('error!');}})
+
+document.querySelector('#number1').addEventListener('click', onclickNumber)
+document.querySelector('#number2').addEventListener('click', onclickNumber)
+document.querySelector('#number3').addEventListener('click', onclickNumber)
+document.querySelector('#number4').addEventListener('click', onclickNumber)
+document.querySelector('#number5').addEventListener('click', onclickNumber)
+document.querySelector('#number6').addEventListener('click', onclickNumber)
+document.querySelector('#number7').addEventListener('click', onclickNumber)
+document.querySelector('#number8').addEventListener('click', onclickNumber)
+document.querySelector('#number9').addEventListener('click', onclickNumber)
+document.querySelector('#number0').addEventListener('click', onclickNumber)
+
+document.querySelector('#plus').addEventListener('click', onclickOperator('+'))
+document.querySelector('#minus').addEventListener('click',onclickOperator('-'))
+document.querySelector('#multiply').addEventListener('click', onclickOperator('*'))
+document.querySelector('#divide').addEventListener('click', onclickOperator('/'))
 document.querySelector('#clear').addEventListener('click', () => 
 {result.value = '';
 printOperator.value = '';
@@ -80,30 +75,48 @@ document.querySelector('#value').addEventListener('click', () =>
 {
     const first = firstNumber;
     const second = secondNumber;
-    // result.value = `${Number(/firstNumber) operator Number(secondNumber)}`;
-})// 버튼 가져오기
+})
 
 
-// 배열화 해서 한번에 클릭이벤트로 만들기
+// // 9. Ternary operator : ?
+// // if를 간단하게 쓸 수 있는
+// // condition ? value1 : value2;
+// console.log(name ==='ellie' ? 'yes' : 'no')
+// // 값이 true면 앞에 꺼 false면 뒤
+// // 가독성을 위해 if나 스위치를 쓰는게 맞다
+// // 간단할때만 쓰는게 좋다.
 
+// (!operator ? firstNumber += event.target.textContent : 
 
-// 클릭이벤트, 함수를 만들어야하는데 
+// const onclickNumber = (event) =>
+        
+//     {if(!operator)
+//         {firstNumber += event.target.textContent}
+//     else
+//         {secondNumber+= event.target.textContent};
+//     result.value += event.target.textContent;}
+    
+// const onclickOperator = (op) => () => 
+//     {if(firstNumber){
+//         operator = op
+//         printOperator.value = op
+//         result.value = ''
+//     }
+//     else{
+//         alert('error!');}
+//     }
 
-
-// 함수 1. operator가 비어있다. firstNumber에 저장
-// operator가 차있다. secondNumber에 저장
-
-// 함수 2. operator에 저장
-
-
-// Array.join() > 배열을 합쳐줌
-
-function A(){
-    console.log('1')
-}
-
-const B = (() => {console.log('hello'); console.log('B')})
-
-const C = ['1','2','3']
-
-C.forEach(B)
+function test() {
+    let result ='';
+    if(!a){
+        result = 'a';
+        result +='b';
+        return result;
+        
+    }   
+    if(!b){
+        result = 'c';
+        result +='b';
+        return result;
+    }
+    }
