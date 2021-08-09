66f699db8e3e34735d7b54fba0d5a730c40030f6
dlxorb96 <80591023+dlxorb96@users.noreply.github.com>
2021-08-09 15:59:05 +0900

    8/9 공부
    버그 없는 계산기 만들기 성공...
    
    1. switch(opeator){
              case '+' : adskmasdkm
                             break;
              case '-' : asd,lasd
                            break;
              default: break;
    }
    === if(operation === '+'){asl,dasld}
          else if (operation ==='-'){asmdkas}
    break를 걸지 않으면 조건에 맞는 케이스부터 아래까지 다 실행이 됨. 그래서 하나만 실행되게 하려면 break를 걸어줘야함. switch문에서 default는 else랑 같음. 차이점은 else는 밑으로 가야하지만 default는 제일 위에 와도 됨. 
    switch문에 익숙해지자
    
    2. #value부분을 나는 '3' + '+' + '4'을 합치는 방법으로 하려고 했음 그래서 백틱으로 접근 했었는데, 이는 eval('3+4')로 접근 가능함. 하지만 이 방법은 사용되지 않는 방법임. 해킹의 위협이 있다. 
    const str = 'ler'
    eval('a' + str + 't'('eval은 위험해')를 하면 'eval은 위험해'가 alert로 실행됨.
    문자열이 숫자로 바뀌고, 코드도 실행시킴.
    
    3. onclickNumber 함수 안에 다양한 if문이 들어갔음. 그 때 겹치는 부분이 있으면
    위에 식이 실행이 되고 밑에 식도 실행이 됨. 하지만 나는 그걸 원하지 않음
    이를 해결하는 방법은 끝에 return;을 해주는 것임. return을 써주면 함수가 종료됨을 알 수 있음.
    
    느낀 점 
    마이너스 넘버 기능 하나 추가하려다 버그가 생기고, 그 버그를 해결하니 다른 버그가 생기고,
    버그에 버그를 무는 버그의 끝을 봤다...
    그래도 끝까지 자리에 앉아서 해결하려고 노력하니 해결이 되네
    짜증난다고 자리 피하지 말고 끝까지 물어 뜯어 보자. 보람도 배가 되어 돌아온다 !!

--- 계산기/main.js
+++ 계산기/main.js
@@ -5,7 +5,7 @@ let firstNumber ='';
 let secondNumber ='';
 let operator='';
 
-
+//  고차함수 만들기
 // const onclickNumber = (number) => () => {
 //         if(!operator){
 //             firstNumber += number
@@ -16,7 +16,7 @@ let operator='';
 //         result.value += number;}
     
 
-
+// 고차함수 중괄호, return 생략 전
 // function onclickNumber(number){
     
     
@@ -27,28 +27,109 @@ let operator='';
 //     result.value += number;};
 // }
 
-
-
-const onclickNumber = (event) =>
+const onclickNumber = (event) =>{
         
-    {if(!operator)
-        {firstNumber += event.target.textContent}
-    else
-        {if(secondNumber){
-            result.value = '';
-        }
-        secondNumber+= event.target.textContent};
-        result.value += event.target.textContent;}
+    if(!operator) { //operator가 없을 때
+        firstNumber += event.target.textContent; // 입력하는 건 퍼넘이 된다 
+        result.value += event.target.textContent;// 
+        return}
+        
+    // operator가 있을때 
+    if(!secondNumber){ // second넘버가 없을 때
+        result.value = ''; // 인풋창을 비운다.
+    }
+    secondNumber+= event.target.textContent;//세컨드넘버에 입력된다.
+    result.value += event.target.textContent;}; // 누르는 걸 인풋에 입력한다.
+         
+
+// const onclickOperator = (op) => () => 
+//     {if(firstNumber){
+//         operator = op
+//         printOperator.value = op
+//     }
+//     else{
+//         alert('error!');}
+//     }
+
+//  버그 수정 전 2가지 숫자만 할 수 있음 두번째 숫자를 할당하고 나서
+// 연산자를 눌러도 secondNumber 뒤에 string으로 더해짐. 
+// 이를 해결 하기위해 2가지 방법으로 2가지 해결을 했음.
+// 1. 두번째 숫자이후 추가 연산을 할 경우 에러를 발생시켰음.
+// 2. 두 수를 연산한 값을 firstNumber로 할당시키고 추가연산이
+// 가능하게 만들었음.
+
+
+//  1번 해결 방안
+// const onclickOperator = (op) => () => 
+//     {if(firstNumber&&!secondNumber){
+//         operator = op
+//         printOperator.value = op
+//     }
+//     else{
+//         alert('error!');}
+//     }
+
+// 2번 해결 방안
+const onclickOperator = (op) => () => {
+    // 마이너스 숫자 만들기 
+    // 1. firstNumber로 들어오는 경우
+    if(op ==='-' && !firstNumber){
+        result.value = ''
+        result.value += '-'
+        firstNumber += '-'
+        return;
+    }
+    // 2. secondNumber로 들어오는 경우
+    if(op ==='-'&& operator ==='-' && !secondNumber){
+        result.value = ''
+        result.value += '-'
+        secondNumber += '-'
+        return;
+    }
     
-const onclickOperator = (op) => () => 
-    {if(firstNumber){
-        operator = op
+    if(result.value === '-'){
+        alert('Please Press Number')
+        return;
+    }
+    if(firstNumber&&!secondNumber){
+    operator = op
+    printOperator.value = op
+    }
+    else if(secondNumber){
+        switch (operator){
+            case '+':
+                result.value = parseInt(firstNumber) + parseInt(secondNumber);
+                firstNumber = result.value
+                secondNumber = ''
+                break;
+            
+            case '-':
+                result.value = parseInt(firstNumber) -parseInt(secondNumber);
+                firstNumber = result.value
+                secondNumber = ''
+                break;
+            case '*' :
+                result.value = parseInt(firstNumber) * parseInt(secondNumber);
+                firstNumber = result.value
+                secondNumber = ''
+                break;
+            case '/' :
+                result.value = parseInt(firstNumber) / parseInt(secondNumber);
+                firstNumber = result.value
+                secondNumber = ''
+                break;
+            default: 
+                break;
+        }
         printOperator.value = op
-        // result.value = ''
+        operator = op
+
     }
     else{
-        alert('error!');}
+        alert('Please Press Number!')
     }
+}
+
 
 document.querySelector('#number1').addEventListener('click', onclickNumber)
 document.querySelector('#number2').addEventListener('click', onclickNumber)
@@ -72,21 +153,74 @@ firstNumber ='';
 secondNumber = ''
 operator = ''})
 document.querySelector('#value').addEventListener('click', () => 
-{
-    const first = firstNumber;
-    const second = secondNumber;
+{   // 1번째 방법 if문을 사용하기
+    // if(secondNumber){
+    //     if(operator === '+'){
+    //         result.value = parseInt(firstNumber) + parseInt(secondNumber)
+            
+    //     }
+    //     else if(operator === '-'){
+    //         result.value = parseInt(firstNumber) - parseInt(secondNumber)
+    //     }
+    //     else if(operator === '*'){
+    //         result.value = parseInt(firstNumber) * parseInt(secondNumber)
+    //     }
+    //     else if(operator === '/'){
+    //         result.value = parseInt(firstNumber) / parseInt(secondNumber)
+    //     }
+    // }
+    // else{
+    //     alert('error!')
+    // }
+    // 2번재 방법 switch 사용하기
+    // 1. firstNumber로 들어오는 경우
+    
+    if(firstNumber&&!secondNumber){
+    operator = op
+    printOperator.value = op
+    }
+    if(secondNumber){
+        switch (operator){
+            case '+':
+                result.value = parseInt(firstNumber) + parseInt(secondNumber);
+                firstNumber = result.value
+                printOperator.value =''
+                operator =''
+                secondNumber = ''
+                break;
+            
+            case '-':
+                result.value = parseInt(firstNumber) -parseInt(secondNumber);
+                firstNumber = result.value
+                printOperator.value =''
+                operator =''
+                secondNumber = ''
+                break;
+            case '*' :
+                result.value = parseInt(firstNumber) * parseInt(secondNumber);
+                firstNumber = result.value
+                printOperator.value =''
+                operator =''
+                secondNumber = ''
+                break;
+            case '/' :
+                result.value = parseInt(firstNumber) / parseInt(secondNumber);
+                firstNumber = result.value
+                printOperator.value =''
+                operator =''
+                secondNumber = ''
+                break;
+            default:
+                break;
+        }}
+    else{
+        alert('Please Press Number!')
+    }
+    
 })
 
 
-// // 9. Ternary operator : ?
-// // if를 간단하게 쓸 수 있는
-// // condition ? value1 : value2;
-// console.log(name ==='ellie' ? 'yes' : 'no')
-// // 값이 true면 앞에 꺼 false면 뒤
-// // 가독성을 위해 if나 스위치를 쓰는게 맞다
-// // 간단할때만 쓰는게 좋다.
 
-// (!operator ? firstNumber += event.target.textContent : 
 
 // const onclickNumber = (event) =>
         
@@ -105,18 +239,3 @@ document.querySelector('#value').addEventListener('click', () =>
 //     else{
 //         alert('error!');}
 //     }
-
-function test() {
-    let result ='';
-    if(!a){
-        result = 'a';
-        result +='b';
-        return result;
-        
-    }   
-    if(!b){
-        result = 'c';
-        result +='b';
-        return result;
-    }
-    }
