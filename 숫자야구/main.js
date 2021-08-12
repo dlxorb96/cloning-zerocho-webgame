557c1eff147682a8a2e9a88c0493b1e5dfa688e7
dlxorb96 <80591023+dlxorb96@users.noreply.github.com>
2021-08-12 12:28:55 +0900

    8/10 공부
    1. array.splice는 array 요소를 제거함으로 push를 먼저하고 splice를 해야 하나 추가되고, 제거됨.
    splice를 먼저하면 제거가 먼저 되기 때문에 버그발생할수있음 !
    
    2. gameNumber.push(totalNumber.splice(1, 1)를 하면 안되는 이유
    splice는 배열 통째로 자르기 때문에 [[1], [2], [3]]이렇게 될 수 있음.

--- 숫자야구/main.js
+++ 숫자야구/main.js
@@ -3,41 +3,45 @@
 const start = document.querySelector('.start');
 const what = document.querySelector('#what');
 const box = document.querySelector('#box')
-const gameInput = document.querySelector('#gameInput');
+const gameForm = document.querySelector('#gameForm');
 
 const HIDDEN_STRING = 'hidden'
-
+// 시작부분 시작하기 누르면 뜨는 거
 function clickEvent(){
 	start.classList.add(HIDDEN_STRING)
 	what.classList.remove(HIDDEN_STRING)
 	box.classList.replace(HIDDEN_STRING, 'box')
-	gameInput.classList.remove(HIDDEN_STRING)
+	gameForm.classList.remove(HIDDEN_STRING)
 }
 
-// let number = Math.floor(Math.random() * 10000)
 
 start.addEventListener('click', clickEvent)
 
-// let number1 = [];
-// let i = 0
-// while(i < 9){
-// 	i++
-// 	number1.push(i);
-// }
+//숫자 랜덤으로 정하기
 
-// console.log(number1)
-
-
-// let number2 =[];
-// for(let j = 0; j<9; j+=1){
-// 	number2.push(j+1);
-// }
-// console.log(number2)
-
-let number = [];
-let i =0
-while(i <9){
-number.push(i)
-i++
+const totalNumber =[];
+//totalNumber를 1,2,3,4 직접 넣을 수도 있지만 반복문으로 나타내기
+for(let i=0; i<9; i++){
+	totalNumber.push(i+1)
 }
-console.log(number)
+
+//gameNubmer를 정해야함.
+const gameNumber =[];
+
+//랜덤 숫자 뽑기
+
+for(let i=0; i<4; i++){
+	const index = Math.floor(Math.random() * totalNumber.length) //0~8까지
+	gameNumber.push(totalNumber[index]) //숫자 넣기
+	totalNumber.splice(index, 1) //숫자 자르기
+	// gameNumber.push(totalNumber.splice(index, 1)) 을 하면 안되는 이유는
+  // splice는 배열 통째로 자르기 때문에 [[1], [2], [3], [4]]와 같은 형태가 된다.
+	// 토탈넘버에서 숫자 잘라서 gameNumber에 집어넣기 4번 반복
+}
+
+function submitEvent(event){
+	event.preventDefault() //기본 이벤트 제출 막기
+	
+}
+
+gameForm.addEventListener('submit', submitEvent)
