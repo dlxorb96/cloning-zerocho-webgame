813121a7a30db392bf1124569bc79ba757da54fe
dlxorb96 <80591023+dlxorb96@users.noreply.github.com>
2021-08-13 22:11:42 +0900

    8/13 공부
    1. new Set()을 하면 중복을 없애준다 123444 > 1234
    
    2. array.length를 하면 개수만큼 나옴 배열만 0부터 시작하는거임.
    
    3. array.include(123) 
    array에 123을 포함하는가 >> return true or false
    
    4. innerHtml, innerText, textContent 차이점
    textContent는 text만 됨 \n 안됨
    innerText는 \n됨 하지만 <br>안됨
    innerHtml은 다 됨

--- 숫자야구/main.js
+++ 숫자야구/main.js
@@ -4,8 +4,11 @@ const start = document.querySelector('.start');
 const what = document.querySelector('#what');
 const box = document.querySelector('#box')
 const gameForm = document.querySelector('#gameForm');
+const $input = document.querySelector('#gameInput');
+const result = document.querySelector('.result')
 
 const HIDDEN_STRING = 'hidden'
+
 // 시작부분 시작하기 누르면 뜨는 거
 function clickEvent(){
 	start.classList.add(HIDDEN_STRING)
@@ -39,9 +42,47 @@ for(let i=0; i<4; i++){
 	// 토탈넘버에서 숫자 잘라서 gameNumber에 집어넣기 4번 반복
 }
 
+const answer = gameNumber.join('') // 배열이었던 랜덤 숫자를 문자열로 바꿈
+
+const tries = [];  //시도하는 거 저장
+
+//숫자 4자리수, 겹치는가 판단하기
+//서밋이벤트에서 inputNumber를 받아와서 인자로 받는다.
+function checkNumber(value){
+  // inputNumber의 길이가 4인가?
+  if(value.length !== 4){
+    return alert("4자리 숫자를 입력해주세요.");}
+  // 중복된 숫자가 있는가?
+  if(new Set(value).size !==4){
+    return alert("중복 없이 입력해주세요");
+  }
+  // 이미 시도한 값인가?
+  if(tries.includes(value)){
+    return alert("이미 시도한 값입니다.");
+  }
+  // 정상적인 입력일 시
+  return true;
+  } 
+
+// 숫자 제출하기
+
 function submitEvent(event){
 	event.preventDefault() //기본 이벤트 제출 막기
-	
-}
+	const inputNumber = $input.value // 적는창 밸류 저장하기
+	$input.value = ''  // 인풋창 다시 비우기
+	if(checkNumber(inputNumber)){
+    //입력값 문제 없음
+    if(inputNumber === answer){
+      result.innerHTML = '1볼 1스트라이크'+'<br/>'+' HomeRun!'
+      return;
+    }
+    if(tries.length >= 9){
+      const message = document.createTextNode(`패배! 정답은${answer}`)
+      result.appendChild(message)
+    }
+
+
+    tries.push(inputNumber);
+  }}
 
 gameForm.addEventListener('submit', submitEvent)
