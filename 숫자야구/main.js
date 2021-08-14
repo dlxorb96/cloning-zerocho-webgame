49e9354377ed8050cf37ccf343d430c966e83b0a
dlxorb96 <80591023+dlxorb96@users.noreply.github.com>
2021-08-14 22:42:40 +0900

    8/14 공부
    1. append와 appenChild 차이점
    append가 appenChild 뒤에 나왔음 보완해 주려고 나왔음
    appnedChild는 message = createTextNode('123')로 만들고서 append(message)를 해야하는 반면에
    append는 문자열은 바로 할 수 있음 append('123', createElement('br'))이런 식으로 그리고 두 가지도 넣을 수 있음.
    
    2. indexOf()는 겹치는 걸 찾아줌 겹치는 게 없으면 -1이 나온다
    겹치는 게 있으면 배열의 몇번째 자리수가 겹치는지 숫자가 나옴 1234.indexOf(3)을 하면 2가 나옴.
    include는 포함되는게 있으면 true or false 
    indexOf는 자릿수까지.

--- 숫자야구/main.js
+++ 숫자야구/main.js
@@ -10,11 +10,11 @@ const result = document.querySelector('.result')
 const HIDDEN_STRING = 'hidden'
 
 // 시작부분 시작하기 누르면 뜨는 거
-function clickEvent(){
-	start.classList.add(HIDDEN_STRING)
-	what.classList.remove(HIDDEN_STRING)
-	box.classList.replace(HIDDEN_STRING, 'box')
-	gameForm.classList.remove(HIDDEN_STRING)
+function clickEvent() {
+  start.classList.add(HIDDEN_STRING)
+  what.classList.remove(HIDDEN_STRING)
+  box.classList.replace(HIDDEN_STRING, 'box')
+  gameForm.classList.remove(HIDDEN_STRING)
 }
 
 
@@ -22,24 +22,24 @@ start.addEventListener('click', clickEvent)
 
 //숫자 랜덤으로 정하기
 
-const totalNumber =[];
+const totalNumber = [];
 //totalNumber를 1,2,3,4 직접 넣을 수도 있지만 반복문으로 나타내기
-for(let i=0; i<9; i++){
-	totalNumber.push(i+1)
+for (let i = 0; i < 9; i++) {
+  totalNumber.push(i + 1)
 }
 
 //gameNubmer를 정해야함.
-const gameNumber =[];
+const gameNumber = [];
 
 //랜덤 숫자 뽑기
 
-for(let i=0; i<4; i++){
-	const index = Math.floor(Math.random() * totalNumber.length) //0~8까지
-	gameNumber.push(totalNumber[index]) //숫자 넣기
-	totalNumber.splice(index, 1) //숫자 자르기
-	// gameNumber.push(totalNumber.splice(index, 1)) 을 하면 안되는 이유는
+for (let i = 0; i < 4; i++) {
+  const index = Math.floor(Math.random() * totalNumber.length) //0~8까지
+  gameNumber.push(totalNumber[index]) //숫자 넣기
+  totalNumber.splice(index, 1) //숫자 자르기
+  // gameNumber.push(totalNumber.splice(index, 1)) 을 하면 안되는 이유는
   // splice는 배열 통째로 자르기 때문에 [[1], [2], [3], [4]]와 같은 형태가 된다.
-	// 토탈넘버에서 숫자 잘라서 gameNumber에 집어넣기 4번 반복
+  // 토탈넘버에서 숫자 잘라서 gameNumber에 집어넣기 4번 반복
 }
 
 const answer = gameNumber.join('') // 배열이었던 랜덤 숫자를 문자열로 바꿈
@@ -48,41 +48,57 @@ const tries = [];  //시도하는 거 저장
 
 //숫자 4자리수, 겹치는가 판단하기
 //서밋이벤트에서 inputNumber를 받아와서 인자로 받는다.
-function checkNumber(value){
+function checkNumber(value) {
   // inputNumber의 길이가 4인가?
-  if(value.length !== 4){
-    return alert("4자리 숫자를 입력해주세요.");}
+  if (value.length !== 4) {
+    return alert("4자리 숫자를 입력해주세요.");
+  }
   // 중복된 숫자가 있는가?
-  if(new Set(value).size !==4){
+  if (new Set(value).size !== 4) {
     return alert("중복 없이 입력해주세요");
   }
   // 이미 시도한 값인가?
-  if(tries.includes(value)){
+  if (tries.includes(value)) {
     return alert("이미 시도한 값입니다.");
   }
   // 정상적인 입력일 시
   return true;
-  } 
+}
 
 // 숫자 제출하기
 
-function submitEvent(event){
-	event.preventDefault() //기본 이벤트 제출 막기
-	const inputNumber = $input.value // 적는창 밸류 저장하기
-	$input.value = ''  // 인풋창 다시 비우기
-	if(checkNumber(inputNumber)){
-    //입력값 문제 없음
-    if(inputNumber === answer){
-      result.innerHTML = '1볼 1스트라이크'+'<br/>'+' HomeRun!'
-      return;
+function submitEvent(event) {
+  event.preventDefault() //기본 이벤트 제출 막기
+  const inputNumber = $input.value // 적는창 밸류 저장하기
+  $input.value = ''  // 인풋창 다시 비우기
+  if (!checkNumber(inputNumber)) {
+    return;
+  }
+  // 맞출시
+  if (inputNumber === answer) {
+    result.innerHTML = '<br/>' + ' HomeRun!'
+    return;
+  }
+  //졌을시
+  if (tries.length >= 9) {
+    const message = document.createTextNode(`패배! 정답은${answer}`)
+    result.appendChild(message)
+    return;
+  }
+  let strike = 0;
+  let ball = 0;
+  for(let i=0; i < gameNumber.length; i++){
+    const index = inputNumber.indexOf(gameNumber[i])
+    //일치하는 숫자를 찾았을 때
+    if(index > -1){
+      (index === i) ? strike +=1 : ball += 1
+      // 자리가 같으면 스트라이크 다르면 볼
     }
-    if(tries.length >= 9){
-      const message = document.createTextNode(`패배! 정답은${answer}`)
-      result.appendChild(message)
-    }
-
-
-    tries.push(inputNumber);
-  }}
+  }
+  result.append(`${inputNumber}: ${strike}스트라이크 ${ball}볼`, document.createElement('br'))
+  tries.push(inputNumber); //입력값을 tries에 집어넣는다.
+}
 
 gameForm.addEventListener('submit', submitEvent)
+
+
