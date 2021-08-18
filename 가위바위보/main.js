b3f211644b6fb380ca3945c7695c491286ddebd4
dlxorb96 <80591023+dlxorb96@users.noreply.github.com>
2021-08-18 23:14:40 +0900

    8/ 18 공부
    1. ,/ 현재폴더
    ../ 부모폴더 ( 상위폴더)
    ../../ 할아버지 폴더
    >>>이런 것들은 상대경로
    
    /rsp.png >> 절대경로 c드라이브에서 출발한다.
    
    2. tag.style.backgroundSize = 'auto 200px'
    200px에 맞춰서 비율에 맞게 크기를 맞춰준다.
    옛날에는 인터넷 익스플로러가 이미지를 10개씩 밖에 못 담았음. 이미지를 하나로 합쳐놨었음, 서버에 이미지를 요청하는 횟수를 줄이는 방법 이미지 스프라이트
    (참고: http://tcpschool.com/css/css_basic_imageSprites)
    
    3. background = 2px 0px 가로 /세로
    padding = 5px 7px 2px 2px 위/ 오/ 아/ 왼
    5px 10px 0px 위,/ 오른쪽 왼쪽 /, 아래
    10px 0px  위,아래 / 오 왼 
    background는 공간개념이 아니라 위치이동 개념 얼마나 이동할거냐
    background사진 한장이라도 지정된 공간을 꽉채운다 backgroundSize를 그래서 계속 해줘야함.
    
    4. else를 쓰는 걸 비추천 함. 오타가 있을경우나 예상치 못한 변수가 있을 경우 통제하기 어려움 else if를 하는 걸 추천
    
    5. rsp = {
    a :1,
    b:2,
    c: 3,}
    const computer = a
    object에서 rsp[computer] << computer의 값이 들어감
    rsp['a']
    1이 되는 거임
    rsp.computer는 안됨 주의할 것 !!
    
    6. setInterval은 재귀 setTimeout으로 대체할 수 있다.
    재귀함수 : 자기 함수 안에서 함수를 다시 호출하는 함수
    
    7. setTimeout과 setInterval은 함수 안에 있는 것들이 실행이 되고 뒤에 초로 다시 시작 되는 것이기 때문에 시간이 정확하게 지켜지지 않을 수도 있다. 하지만 대부분 지켜지기 때문에 쓴다. setInterval은 시간을 지키려 노력함. setTimeout은 안 함
    
    8. const id = setInterval(func, 1000)
    clearInterval(id)로 작동을 멈출 수 있다.
    예전 타이머로 지금 타이머를 지울 수 없다. 타이머마다 id를 가지고 있다. new개념으로 생각하면 될듯.
    그래서 타이머를 지우고 다시 같은 이름을 가진 타이머를 호출할 때, 다시 아이디를 이용하여 지울 경우에는 다시 선언 해줘야 한다. 함수만 호출해줘선 안됨. 
    
    9. flag변수 true /false에 따라 코드의 실행 여부를 결정해주는 변수.
    
    removeEventListener보다 flag함수를 추천하는 이유
    func(1) ===func(1)은 false
    (() => {}) === (() => {}) 은 false 
    [] === [] 은 false
    {} === {}은 false
    객체끼리는 비교 불가능임 어디에 선언해서 비교를 해줘야함.
    const a = func(1)
    func(1 ) === a true
    a === a true
    
    10. 코드가 길어질 때에는 숫자로 바꿔서 생각해보자. 그리고 규칙을 찾아보자.
    단 이런 경우에는 주석이 길어질 수 있다.
    
    11. or 문이 길어진다면
    if(diff === A || diff ===B ||diff ===C ||diff ===D ||)
    if([A,B,C,D].includes(diff))를 통해 간결하고 명확하게 할 수 있다.
    if([A,B,C,D].indexOf(diff) > -1)도 가능

---
+++ 가위바위보/main.js
@@ -0,0 +1,120 @@
+'use strict';
+
+const computer = document.querySelector('#computer');
+const result = document.querySelector('#result')
+const rock = document.querySelector('#rock')
+const scissors = document.querySelector('#scissors')
+const paper = document.querySelector('#paper')
+
+const IMG_URL = './rsp.png';
+computer.style.background = `url(${IMG_URL})0 0`;
+computer.style.backgroundSize = 'auto 200px';
+
+//초마다 이미지의 위치가 바뀌도록 설정하자
+
+const RSPs = {
+  scissors : {
+    background : `url(${IMG_URL})0 0`,
+    backgroundsize: 'auto 200px',
+  },
+  rock : {
+    background : `url(${IMG_URL})-222px 0`,
+    backgroundsize: 'auto 200px',
+  },
+  paper :  {
+    background : `url(${IMG_URL})-444px 0`,
+    backgroundsize: 'auto 200px',
+  },
+}
+let computerChoice = 'rock'
+function changeComputerHand(){
+  if(computerChoice ==='rock'){ // 가위일 때
+    computerChoice = 'scissors'
+    computer.style.background = RSPs.scissors.background;
+    computer.style.backgroundSize = RSPs.scissors.backgroundsize;
+  }else if(computerChoice === 'scissors'){ // 바위일 때
+    computerChoice = 'paper'
+    computer.style.background = RSPs.paper.background;
+    computer.style.backgroundSize = RSPs.paper.backgroundsize;
+  }else if(computerChoice === 'paper'){ //보일 때
+    computerChoice = 'rock'
+    computer.style.background = RSPs.rock.background;
+    computer.style.backgroundSize = RSPs.rock.backgroundsize;
+  }
+}
+
+let intervalName = setInterval(changeComputerHand, 50);
+
+//여기서 클릭했을 때 멈추는 이벤트를 만든다
+
+//이벤트리스너를 지웠다가 다시 만드는 방법
+
+// function clickEvent(){
+//   clearInterval(intervalName) // 클릭했을 때 기존의 inerval을 멈춘다.
+//   //1초후에 다시 인터벌을 시작한다.
+//   //클릭이벤트를 지웠다가 
+//   rock.removeEventListener('click', clickEvent);
+//   scissors.removeEventListener('click', clickEvent);
+//   paper.removeEventListener('click', clickEvent);
+//   //클릭했을 때 버그 발생하는 거 수정하기 
+//   setTimeout(() =>{
+//     clearInterval(intervalName)
+//     //1초후에 다시 만들기
+//     rock.addEventListener('click', clickEvent);
+//     scissors.addEventListener('click', clickEvent);
+//     paper.addEventListener('click', clickEvent);
+//     intervalName = setInterval(changeComputerHand, 50);}
+//   ,1000)
+  
+// }
+
+const score = {
+  rock : 0,
+  scissors: 1,
+  paper: 2,
+}
+let message = '';
+let scoreMessage = '';
+
+// 이벤트리스너 대신 if문을 통해서 만드는 방법
+let flagF = true;
+function clickEvent(event){
+  if(flagF){
+  clearInterval(intervalName) // 클릭했을 때 기존의 inerval을 멈춘다.
+  flagF = false   // 플래그함수를 false로 만들어준다.
+  const userChoice = event.target.id === 'rock' ? 'rock': 
+  event.target.id === 'scissors' ? 'scissors' : 'paper';
+  
+  const computerScore = score[computerChoice];
+  const userScore = score[userChoice];
+  const diff = computerScore -userScore
+  console.log(computerChoice, userChoice, diff)
+  if([-2, 1].includes(diff)){
+    scoreMessage += 1
+    message = '승리';
+  }else if([2, -1].includes(diff)){
+    scoreMessage -= 1;
+    message = '패배'
+  }else if([0].includes(diff)){
+    message = '무승부'
+  }
+  result.textContent = `점수 : ${scoreMessage} ${message}`
+
+   //1초후에 다시 인터벌을 시작한다.
+  setTimeout(() =>{
+    flagF = true
+    intervalName = setInterval(changeComputerHand, 50);}
+  ,1000)
+  }
+}
+
+
+// if()
+
+
+rock.addEventListener('click', clickEvent);
+scissors.addEventListener('click', clickEvent);
+paper.addEventListener('click', clickEvent);
+
+
+//클릭했을 때 버그 발생하는 거 수정하기 
