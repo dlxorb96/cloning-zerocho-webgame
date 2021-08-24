91062cf8af1c319cf5c7153ebc893292a1b1ce68
dlxorb96 <80591023+dlxorb96@users.noreply.github.com>
2021-08-24 23:16:41 +0900

    Add files via upload

--- 틱택토/main.js
+++ 틱택토/main.js
@@ -4,13 +4,40 @@
 
 const $table = document.createElement('table')
 const { body } = document
-
+const result = document.createElement('div')
 let turn = 'X'
+
+function checkWinner(target){
+  let rowIndex;
+  let cellIndex;
+  rows.forEach((row, ri) =>{
+    row.forEach((cell , ci) => {
+      if(cell === target){
+        rowIndex = ri;
+        cellIndex = ci;
+        console.log(ri, ci)
+      }
+    })
+  })
+  let winner = false
+  if(rows[rowIndex][0].textContent === turn&&
+  rows[rowIndex][1].textContent === turn&&
+  rows[rowIndex][2].textContent === turn){
+    console.log('1')
+    winner = true
+  }
+  return winner;
+  // console.log(rows[rowIndex][cellIndex])
+  console.log(rows[0][cellIndex],rows[1][cellIndex],rows[2][cellIndex])
+}
 
 function checkFunc(event){
   if(event.target.textContent === ''){
   event.target.textContent = turn
   turn = turn === 'X' ? 'O' : 'X'
+  if(checkWinner(event.target)){
+    result.textContent = 누구의승리
+  }
   }
   //   if(turn === 'O'){
   //     event.target.textContent = 'O'
