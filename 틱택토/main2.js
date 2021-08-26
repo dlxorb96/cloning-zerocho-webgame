73f60ee6ee0708f05ec986b17d53db888f4c76a9
dlxorb96 <80591023+dlxorb96@users.noreply.github.com>
2021-08-26 12:37:02 +0900

    8/26 공부
    1. 표안에 있는 td들 변수 선언하기 
      -1.  이중반복문을 통해 나타내기
            forEach((row, ri) =>{ 
                   row.forEach(cell, ci){
                          if(target === cell
                            const cellIndex = ci
                            const rowIndex = ri)}} 
     -2. html의 속성을 통해 나타내기
    const cellIndex = target.cellIndex
    const rowIndex = target.parentNode.rowIndex
    td는 cellIndex를 가지고 tr은 rowIndex를 가진다.( property)
    
    2.  유사배열객체 배열화하기
    Array.from($tabel.childeren)하면 배열로 바꿔줌
    
    3. 이중반복문은 비효율적이다. 빈 칸을 찾았을 때 멈춰야하는데 전부 다 돌림 >> 알고리즘 문제의 탈락 원인
    array.every(()=>{}) 하나라도 실패 false 다 통과면 true
    반대로 array.some(()=>{}) 하나라도 성공이면 true 
    
    4. array.flat()을 하면 n차원 배열을 n-1차원 배열로 만들어 준다.
    
    5, setTimeout()은 에러가 잘 발생한다. 항상 주의하고, flag함수를 통해 개선할 수 있다.
    
    6. 현재 태그의 부모태그를 찾고 싶으면 parentNode 자식 태그 children

--- 틱택토/main2.js
+++ 틱택토/main2.js
@@ -1,85 +1,91 @@
 'use strict';
 
-
-
 const $table = document.createElement('table')
 const { body } = document
 const result = document.createElement('div')
-let turn = 'O'
 const rows = []
+
+let turn = 'O'
 
 function checkWinner(target){
   //이중 반복문으로 뽑아왔던 rowIndex, cellIndex를 쉽게 표현하기
-  const rowIndex = target.parentNode.rowIndex;
-  const cellIndex = target.cellIndex;
+  const cellIndex = target.cellIndex
+  const rowIndex = target.parentNode.rowIndex
 
-  let hasWinner = false
-  
+
+  let winner = false
+
   //가로줄 판단하기
   if(rows[rowIndex][0].textContent === turn&&
   rows[rowIndex][1].textContent === turn&&
   rows[rowIndex][2].textContent === turn){
-    hasWinner = true
+    winner = true
   }
   //세로줄 판단하기
   if(rows[0][cellIndex].textContent ===turn&&
     rows[1][cellIndex].textContent ===turn&&
     rows[2][cellIndex].textContent ===turn){
-      hasWinner = true
+      winner = true
     }
   //대각선 판단하기
   if(rows[0][0].textContent ===turn&&
     rows[1][1].textContent ===turn&&
     rows[2][2].textContent ===turn){
-      hasWinner = true
+      winner = true
     }
   //대각선 판단하기
   if(rows[0][2].textContent ===turn&&
   rows[1][1].textContent ===turn&&
   rows[2][0].textContent ===turn){
-    hasWinner = true
+    winner = true
   }
-  return hasWinner;
+  return winner;
 }
 
-function checkWinnerAndDraw(target){
-  const hasWinner = checkWinner(target)
-  //승자가 있으면
+function winAndDraw(target){
+  //승리판단하기
   if(checkWinner(target)){
     result.textContent = `${turn}의 승리`
     //게임 끝난 후에도 클릭되는 버그 고치기
     $table.removeEventListener('click', checkFunc)
+    console.log(target)
     return;
   }
 
   //무승부 만들기 1번 방법
   const draw = rows.flat().every((cell) => cell.textContent)
-  
   if(draw){
     result.textContent = '무승부'
     return;
   }
-  //턴 넘기기
   turn = turn === 'X' ? 'O' : 'X'
+
 }
 
-
+let clickable = true
 function checkFunc(event){
+  if(!clickable){
+    return;
+  }
   //빈칸인 경우에만 가능하게 만들기
   if(event.target.textContent === ''){
     event.target.textContent = turn
-    checkWinnerAndDraw(event.target)
-  
-    //컴퓨터 차례
-    if(turn ==='X'){
-      const emptyCells = rows.flat().filter((t)=> !t.textContent)
-      const randomCell = emptyCells[Math.floor(Math.random() * (emptyCells.length-1))]
-      randomCell.textContent = 'X'
-      checkWinnerAndDraw(randomCell)
-    }
-  }
-}  
+    winAndDraw(event.target)
 
+    //컴퓨터 차례 만들기
+    clickable = false
+    setTimeout(() => {
+      if(turn ==='X'){
+        const computerFilter = rows.flat().filter((t)=> !t.textContent)
+        const computerTurn = computerFilter[Math.floor(Math.random() * (computerFilter.length-1))]
+        computerTurn.textContent = turn;
+        winAndDraw(computerTurn)
+        clickable = true
+      }
+    }, 1500)
+  
+  }
+}
 
 for(let i = 0; i<3; i++){
 	const $tr = document.createElement('tr');
